cd4938f621e2de1e2af8ede5b10fd2481393d89f
Mei-Hui Su <mei@MeiPro.local>
2022-09-27 23:38:21 -0700

    add additional removal of artificial entries

--- data/remove_fake_data.py
+++ data/remove_fake_data.py
@@ -42,49 +42,6 @@ def no_need_val(d_val,s_val) :
     else:
       return 1
 
-def no_need(oline,dep_i) :
-    global tmp
-    l=oline.split(",")
-    vs=float(l[3].strip())
-    vp=float(l[4].strip())
-    if (dep_i == 0) :
-        if ((vs == 5.5) & (vp == 3.14) ):
-          return 0
-    elif (dep_i == 1) :
-        if ((vs == 5.52)  & (vp == 3.16) ):
-          return 0
-    elif ( dep_i == 2 ): 
-        if ((vs == 5.55)  & (vp == 3.17) ):
-          return 0
-    elif ( dep_i == 3 ): 
-        if ((vs == 5.57)  & (vp == 3.18) ):
-          return 0
-    elif ( dep_i == 4 ): 
-        if ((vs == 5.6)  & (vp == 3.2) ):
-          return 0
-    elif ( dep_i == 5 ): 
-        if ((vs == 5.63)  & (vp == 3.22) ):
-          return 0
-    elif ( dep_i == 6 ): 
-        if ((vs == 5.67)  & (vp == 3.24) ):
-          return 0
-    elif ( dep_i == 7 ): 
-        if ((vs == 5.7)  & (vp == 3.26) ):
-          return 0
-    elif ( dep_i == 8 ): 
-        if ((vs == 5.73)  & (vp == 3.28) ):
-          return 0
-    elif ( dep_i == 9 ): 
-        if ((vs == 5.76)  & (vp == 3.29) ):
-          return 0
-    elif ( dep_i == 10 ): 
-        if ((vs == 5.8)  & (vp == 3.31) ):
-          return 0
-    else:
-      return 1
-
-    return 1
-
 #-118.17,32.38,-1.50,5.50,3.14
 # to
 #-118.17,32.38,-1.50,-9.999,-9.999
@@ -123,6 +80,8 @@ def main():
 
     rewrite_cnt=0
     no_rewrite_cnt = 0
+    track_fake_1.5 = []  ## should have 6862 of these
+    
 
     d_val= -1.0 * (dep_list[dep_i]*1000) 
     for oline in olds:
@@ -134,14 +93,23 @@ def main():
           if(surf_i == 0) : 
              print(dep_list[dep_i])
              print("  first one :",oline.strip())
-#         if no_need_val(d_val,s_val) :
-          if no_need(oline,dep_i) :
+          if( (dep_i > 0) && (dep_i <= 2) ) :
+            if (track_fake_1.5[surf_i]==1) :
+              nline=rewrite_fake(oline)
+              f_new.write(nline)
+              rewrite_cnt=rewrite_cnt+1
+              continue 
+          if no_need_val(d_val,s_val) :
             ## no change
             f_new.write(oline)
+            if(dep_i == 0) :
+              track_fake_1.5.append(0);
             no_rewrite_cnt=no_rewrite_cnt+1
           else:
             ## rewrite vp/vs to -9999.000
             nline=rewrite_fake(oline)
+            if(dep_i == 0) :
+              track_fake_1.5.append(1);
             f_new.write(nline)
             rewrite_cnt=rewrite_cnt+1
           surf_i=surf_i+1
